5195440e5ffa917d437c9f14971233d3a09f3f91
Krossom <salinasjoost@gmail.com>
2018-09-29 20:22:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hi | This is mine now</title>
+</head>
+<body>
+    This domain has been taked over.
+    @unkn0w3d
+</body>
+</html>
